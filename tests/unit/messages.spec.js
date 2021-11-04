5c3167eae7cf4573a332ef98c5daa8a5aa3a6439
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-04 03:09:09 +0000

    Look for unordered list

--- tests/unit/messages.spec.js
+++ tests/unit/messages.spec.js
@@ -2,11 +2,15 @@ import { shallowMount } from '@vue/test-utils'
 import Messages from '@/components/Messages.vue'
 
 describe('Messages.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+  it('renders props.title when passed', () => {
+    const title = 'new message'
     const wrapper = shallowMount(Messages, {
-      propsData: { msg }
+      propsData: { title }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toMatch(title)
+  })
+  it('renders the unordered list', () => {
+    const wrapper = shallowMount(Messages)
+    expect(wrapper.find('ul').exists()).toBeTruthy()
   })
 })
