f3a02de25c508c3bd6c1987c1451fb59a43ce73e
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-04 19:26:21 +0000

    Install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <div id="app">
-    <Messages title="Messages" />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main class="ml-2">
+      <Messages title="Messages" />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-  import Messages from './components/Messages.vue'
+  import Messages from './components/Messages';
 
   export default {
     name: 'App',
+
     components: {
-      Messages
-    }
-  }
+      Messages,
+    },
+
+    data: () => ({
+      //
+    }),
+  };
 </script>
-
-<style>
-
-</style>
