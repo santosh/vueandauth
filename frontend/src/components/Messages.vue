10c321f14199297bd92dae6e506e8739b43c9c4a
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-04 21:46:37 +0000

    Move code to frontend/

---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h4>{{ title }}</h4>
+    <v-list>
+      <v-list-item-group>
+        <v-list-item
+          v-for="(message, index) in messages"
+          :key="index">
+          <v-list-item-content>
+            <v-list-item-title v-text="message"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Messages',
+    props: ['title'],
+    data() {
+      return {
+        messages: ['1st', '2nd']
+      }
+    }
+  }
+</script>
