aa547d138baf524960929f4920ab91593767e793
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-04 21:56:25 +0000

    Serve messages from /messages

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('test'))
+app.get('/messages', (req, res) => {
+  const messages = ['hello', 'hi', 'its working']
+  res.send(messages)
+})
 
 app.listen(port, () => console.log('app running'))
