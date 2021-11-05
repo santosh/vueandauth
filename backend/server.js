6454dec663becb08bdc1dfaac52b872a25506fb8
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-05 15:06:40 +0000

    Add to messages array on POST

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,17 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const messages = ['hello', 'hi', 'its working']
+
 app.get('/messages', (req, res) => {
-  const messages = ['hello', 'hi', 'its working']
   res.send(messages)
 })
 
 app.post('/messages', (req, res) => {
-  console.log(req.body);
-  res.send({})
+  let msg = req.body
+  console.log(msg);
+  messages.push(msg.message)
+  res.send(msg)
 })
 
 app.listen(port, () => console.log('app running'))
