1e8ec997c4f1b15807e649c6da4535f518733ab2
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-05 06:51:47 +0000

    Serve data to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.get('/messages', (req, res) => {
   const messages = ['hello', 'hi', 'its working']
