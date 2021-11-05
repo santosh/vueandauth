dd70f50dcdd01134d2a1b826aa07f17d77df8cb7
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-05 14:58:12 +0000

    Add /messages for POST

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,17 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/messages', (req, res) => {
   const messages = ['hello', 'hi', 'its working']
   res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+  console.log(req.body);
+  res.send({})
 })
 
 app.listen(port, () => console.log('app running'))
